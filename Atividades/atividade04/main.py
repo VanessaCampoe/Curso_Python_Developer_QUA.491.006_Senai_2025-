259ac94fafbd12c0c39e83308f50341e62d67d69
VanessaCampoe <vanessacampoe@gmail.com>
2025-06-16 21:38:19 -0300

    3 commit Atividade pi

---
+++ Atividades/atividade04/main.py
@@ -0,0 +1,12 @@
+'''# atividade :crie um programa com o seguinta menu:
+calcular  area de um circulo 
+- calcular o tamnaho de um circunferencencia 
+- sair do programa
+'''
+
+import math
+
+raio = float(input("Digite o raio do círculo: "))
+area = math.pi * (raio ** 2)
+print(f"A área do círculo é: {area }")
+#tt
