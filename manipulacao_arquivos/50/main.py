24e16d7a4bc5f3c5e0fe74f5cc18c985022fd853
VanessaCampoe <vanessacampoe2@gmail.com>
2025-07-03 21:48:46 -0300

    commit json 03/07/2025

---
+++ manipulacao_arquivos/50/main.py
@@ -0,0 +1,62 @@
+import json
+import os
+usuarios= []
+novo_arquivo = ""
+while True:
+    usuario = {}
+   
+    print("1 - Cadastrar novo usuarios .")
+    print("2 - salvar arquivo JSON .")
+    print("3 - Fazer leitura do json .")
+    print("4 - sair do programa.")
+    opcao = input("Informe a opção: ").strip()
+    os.system('cls' if os.name == 'nt' else 'clear')
+    match opcao:
+        
+        case '1':
+            usuario['nome'] = input("Informe o nome: ").strip().title()
+            usuario['idade'] = int(input("Informe a idade: "))
+            usuario['email'] = input("Informe o email: ").strip().lower()
+
+            usuarios.append(usuario)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            
+            print(f" Usuário cadastrado com sucesso!")
+            continue
+            
+            
+        case '2': 
+            
+            novo_arquivo = input("Informe o nome do arquivo (sem extensão): ").strip().lower()
+            with open(f"50/{novo_arquivo}.json", "w", encoding='utf-8') as f:
+                json.dump(usuarios, f, ensure_ascii=False, indent=4)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print(f" Arquivo salvo com sucesso !")
+            continue
+         
+            
+        case '3':
+            if not novo_arquivo:
+                novo_arquivo = input("Informe o nome do arquivo : ").strip().lower()
+                        
+            with open(f"50/{novo_arquivo}.json", "r", encoding='utf-8') as f:
+                dados = json.load(f)
+            print(f"{'-'*20} LISTA DE USUÁRIOS {'-'*20}\n")
+            for usuario in dados:
+                for chave in usuario:
+                    print(f"{chave.capitalize()}: {usuario.get(chave)}")
+                    
+                print(f"{'-'*40}\n")
+                continue
+        
+            
+        case '4':
+            
+            print(" Programa encerrado.")
+            break
+        case _:
+            print (" Opção inválida, tente novamente.")
+          
+        
+    
+        
