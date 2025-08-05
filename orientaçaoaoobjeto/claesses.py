5710cff0563ff122c796622e23342d634df9c790
VanessaCampoe <vanessacampoe2@gmail.com>
2025-08-04 21:36:27 -0300

    commit 04/08/2025

---
+++ orientaçaoaoobjeto/claesses.py
@@ -0,0 +1,31 @@
+class Conta:
+    # Construtor
+    def __init__(self, titular, cpf, agencia, num_conta, saldo):
+        # ATRIBUTOS
+        self.titular = titular
+        self.cpf = cpf
+        self.agencia = agencia
+        self.num_conta = num_conta
+        self.saldo = saldo
+
+
+    # METODOS
+
+    def consultar_dados(self):
+        print(f"Titular: {self.titular}")
+        print(f"CPF: {self.cpf}")
+        print(f"Agencia: {self.agencia}")
+        print(f"Conta: {self.num_conta}")
+        print(f"Saldo: R$ {self.saldo:.2f}")
+
+
+    def depositar(self, valor):
+        self.saldo += valor
+        return self.saldo
+    
+    def sacar(self, valor):
+        self.saldo -= valor
+        return self.saldo
+    
+    def extrato(self):
+        ...
